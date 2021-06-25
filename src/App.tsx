3394308f512a7ab72546bbc6b7288ade9739c915
royeradames <royeraadames@gmail.com>
2021-06-25 19:56:01 -0400

    📦add general class name to transform article

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
             <h2 className="main__title transform__title">
               Transform your brand
             </h2>
-            <p className="transform__message">
+            <p className="main__message transform__message">
               We are a full-service creative agency specializing in helping
               brands grow fast. Engage your clients through compelling visuals
               that do most of the marketing for you.
